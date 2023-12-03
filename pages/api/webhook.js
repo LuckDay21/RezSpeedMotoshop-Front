b076d6d30215e83f22e48f0ab4827650187e0482
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-12-03 13:00:04 +0700

    feat: add stipe webhook

---
+++ pages/api/webhook.js
@@ -0,0 +1,50 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Order } from "@/models/Order";
+const stripe = require("stripe")(process.env.STRIPE_SK);
+import { buffer } from "micro";
+
+const endpointSecret =
+  "whsec_c0929050388a83ffd8b5509153ddd89359dad8ed8324278e6e17fa28ca0addbe";
+
+export default async function handler(req, res) {
+  await mongooseConnect();
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await buffer(req),
+      sig,
+      endpointSecret
+    );
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const data = event.data.object;
+      const orderId = data.metadata.orderId;
+      const paid = data.payment_status === "paid";
+      if (orderId && paid) {
+        await Order.findByIdAndUpdate(orderId, {
+          paid: true,
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  res.status(200).send("ok");
+}
+
+export const config = {
+  api: { bodyParser: false },
+};
+
+// lively-talent-famed-noble
+// acct_1OHRARF2c0HCbxkd
