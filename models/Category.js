d62c5bc3f83311b27f5e025dc8b6ed7697880b08
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-12-05 14:12:07 +0700

    feat: add categories page

---
+++ models/Category.js
@@ -0,0 +1,18 @@
+// import mongoose, { Schema, model, models } from "mongoose";
+
+// const CategorySchema = new Schema({
+//   name: { type: String, required: true },
+//   parent: { type: mongoose.Types.ObjectId, ref: "Category" },
+//   properties: [{ type: Object }],
+// });
+
+// export const Category = models?.Category || model("Category", CategorySchema);
+import mongoose, { model, models, Schema } from "mongoose";
+
+const CategorySchema = new Schema({
+  name: { type: String, required: true },
+  parent: { type: mongoose.Types.ObjectId, ref: "Category" },
+  properties: [{ type: Object }],
+});
+
+export const Category = models?.Category || model("Category", CategorySchema);
