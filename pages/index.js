e9d7e7f202779163b3d240b6ce70bc6d45cf7290
LuckDay21 <95541913+LuckDay21@users.noreply.github.com>
2023-12-09 01:06:45 +0700

    feat: add featured product setting

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import NewProducts from "@/components/NewProducts";
 import { WishedProduct } from "@/models/WishedProduct";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-// import {Setting} from "@/models/Setting";
+import { Setting } from "@/models/Setting";
 
 export default function HomePage({
   featuredProduct,
@@ -23,8 +23,11 @@ export default function HomePage({
 }
 
 export async function getServerSideProps(ctx) {
-  const featuredProductId = "6556284c8b5c15ab941ea5cb";
   await mongooseConnect();
+  const featuredProductSetting = await Setting.findOne({
+    name: "featuredProductId",
+  });
+  const featuredProductId = featuredProductSetting.value;
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, {
     sort: { _id: -1 },
@@ -45,44 +48,3 @@ export async function getServerSideProps(ctx) {
     },
   };
 }
-// import Header from "@/components/Header";
-// import Featured from "@/components/Featured";
-// import {Product} from "@/models/Product";
-// import {mongooseConnect} from "@/lib/mongoose";
-// import NewProducts from "@/components/NewProducts";
-// import {WishedProduct} from "@/models/WishedProduct";
-// import {getServerSession} from "next-auth";
-// import {authOptions} from "@/pages/api/auth/[...nextauth]";
-// import {Setting} from "@/models/Setting";
-
-// export default function HomePage({featuredProduct,newProducts,wishedNewProducts}) {
-//   return (
-//     <div>
-//       <Header />
-//       <Featured product={featuredProduct} />
-//       <NewProducts products={newProducts} wishedProducts={wishedNewProducts} />
-//     </div>
-//   );
-// }
-
-// export async function getServerSideProps(ctx) {
-//   await mongooseConnect();
-//   const featuredProductSetting = await Setting.findOne({name:'featuredProductId'});
-//   const featuredProductId = featuredProductSetting.value;
-//   const featuredProduct = await Product.findById(featuredProductId);
-//   const newProducts = await Product.find({}, null, {sort: {'_id':-1}, limit:10});
-//   const session = await getServerSession(ctx.req, ctx.res, authOptions);
-//   const wishedNewProducts = session?.user
-//     ? await WishedProduct.find({
-//         userEmail:session.user.email,
-//         product: newProducts.map(p => p._id.toString()),
-//       })
-//     : [];
-//   return {
-//     props: {
-//       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
-//       newProducts: JSON.parse(JSON.stringify(newProducts)),
-//       wishedNewProducts: wishedNewProducts.map(i => i.product.toString()),
-//     },
-//   };
-// }
